45c9b2b07c4d60006358e09423bb187e2b67ebd4
Riku Inoue <riku.inoue@shiroyagi.co.jp>
2020-05-21 08:02:59 +0900

    Implement blink all LEDs

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,46 @@ extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to c
 // extern crate panic_itm; // logs messages over ITM; requires ITM support
 // extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 
-use cortex_m::asm;
+use cortex_m;
 use cortex_m_rt::entry;
+use stm32f4xx_hal::{
+    prelude::*,
+    prelude::_embedded_hal_digital_v2_OutputPin as OutputPin,
+    gpio::gpiod::Parts,
+    delay::Delay,
+    stm32,
+};
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+    let dp: stm32::Peripherals = stm32::Peripherals::take().unwrap();
+    let cp: cortex_m::Peripherals = cortex_m::peripheral::Peripherals::take().unwrap();
+
+    let gpiod: Parts = dp.GPIOD.split();
+
+    let mut green = gpiod.pd12.into_push_pull_output();
+    let mut orange = gpiod.pd13.into_push_pull_output();
+    let mut red = gpiod.pd14.into_push_pull_output();
+    let mut blue = gpiod.pd15.into_push_pull_output();
+
+    let rcc = dp.RCC.constrain();
+    let clocks = rcc.cfgr.sysclk(48.mhz()).freeze();
+
+    let mut delay = Delay::new(cp.SYST, clocks);
+
+    let delay_ms= 500_u32;
 
     loop {
-        // your code goes here
+        blink_led(&mut green, &mut delay, delay_ms);
+        blink_led(&mut orange, &mut delay, delay_ms);
+        blink_led(&mut red, &mut delay, delay_ms);
+        blink_led(&mut blue, &mut delay, delay_ms);
     }
+}
+
+fn blink_led<T: OutputPin>(led: &mut T, delay: &mut Delay, delay_ms: u32) -> () {
+    led.set_high();
+    delay.delay_ms(delay_ms);
+    led.set_low();
+    delay.delay_ms(delay_ms);
 }
